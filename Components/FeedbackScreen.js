cc1ee553a7472ce4281da7ad9bf29456c4d053ea
Robert L <1998.robert@gmail.com>
2018-08-01 16:14:46 -0700

    deleted more unecessary files

---
+++ Components/FeedbackScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button
+} from 'react-native';
+import styles from './Styles'
+
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+
+class FeedbackScreen extends React.Component {
+  static navigationOptions ={
+    title: 'Feedback'
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      message:''
+    }
+  }
+
+  press() {
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.message}</Text>
+
+      </View>
+    )
+  }
+}
+
+export default LoginScreen;
