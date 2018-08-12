d152a682ae8e72966dafa3aa4e062b18de3186b2
Charles Deng <Charlesdeng98@gmail.com>
2018-08-11 17:52:29 -0700

    fixed product

--- Components/ProductScreen.js
+++ Components/ProductScreen.js
@@ -29,7 +29,7 @@ const HEADER_MIN_HEIGHT = 0;
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 
-export default class ProductScreen extends React.Component {
+class ProductScreen extends React.Component {
   //Location  Favorites,foods,home, history, search?
   static navigationOptions =({navigation}) => {
     const {state} = navigation
@@ -38,6 +38,7 @@ export default class ProductScreen extends React.Component {
     }
 
   };
+
   constructor(props) {
     super(props);
     this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -57,11 +58,11 @@ export default class ProductScreen extends React.Component {
   }
 
   add(){
-    this.setState({quantity:this.state.quantity++})
+    this.setState({quantity:this.state.quantity+1})
   }
 
   subtract(){
-    this.setState({quantity:this.state.quantity--})
+    this.state.quantity > 0 ? this.setState({quantity:this.state.quantity-1}):null
   }
   componentDidMount() {
     console.log("I have reached the Product Screen");
@@ -70,6 +71,9 @@ export default class ProductScreen extends React.Component {
     this.setState({item:item, image: item.imgURI, price: item.price, description: item.description, aisle:item.aisle, name:item.name})
 
   }
+
+
+
   createCategory (cat1,cat2) {
     return  (
       <View style={{flex:1, justifyContent:'flex-start', alignItems:'center',flexDirection:'row',justifyContent:'center'}}>
@@ -100,6 +104,7 @@ export default class ProductScreen extends React.Component {
       console.log(err);
     }
   }
+
   render() {
     const scrollY = Animated.add(
       this.state.scrollY,
@@ -111,18 +116,18 @@ export default class ProductScreen extends React.Component {
       extrapolate: 'clamp',
     });
     const imageOpacity = this.state.scrollY.interpolate({
-      inputRange: [0,  HEADER_SCROLL_DISTANCE],
-      outputRange: [1, 1],
-      extrapolate: 'clamp',
-    });
+  inputRange: [0,  HEADER_SCROLL_DISTANCE],
+  outputRange: [1, 1],
+  extrapolate: 'clamp',
+});
 
-    const imageTranslate = this.state.scrollY.interpolate({
-      inputRange: [0, HEADER_SCROLL_DISTANCE/15,HEADER_SCROLL_DISTANCE/3,HEADER_SCROLL_DISTANCE],
-      outputRange: [0, 50, 60, 100],
-      extrapolate: 'clamp',
-    });
+const imageTranslate = this.state.scrollY.interpolate({
+  inputRange: [0, HEADER_SCROLL_DISTANCE/15,HEADER_SCROLL_DISTANCE/3,HEADER_SCROLL_DISTANCE],
+  outputRange: [0, 50, 60, 100],
+  extrapolate: 'clamp',
+});
 
-    const titleScale = scrollY.interpolate({
+const titleScale = scrollY.interpolate({
       inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
       outputRange: [1, 1, 1],
       extrapolate: 'clamp',
@@ -137,14 +142,21 @@ export default class ProductScreen extends React.Component {
       outputRange: ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(0, 0, 255)'],
       extrapolate: 'clamp',
     });
+
+
+
+    console.log(this.state)
+
     const {name,price,image,description, aisle,item} = this.state
+
     return (
-      <View style={[styles.fill]}>
+      <View style={[styles.fill,{backgroundColor:'white'}]}>
         <StatusBar
          translucent
          barStyle="light-content"
          backgroundColor="rgba(0, 0, 0, 0.251)"
        />
+
           <Animated.ScrollView
              scrollEventThrottle={1}
              style={{
@@ -174,7 +186,9 @@ export default class ProductScreen extends React.Component {
             y: -HEADER_MAX_HEIGHT,
           }}
           >
-            <View style={[styles.scrollViewContent, {marginTop: 105, width: SCREEN_WIDTH}]}>
+
+            <View style={[styles.scrollViewContent,  {marginTop: 105, width: SCREEN_WIDTH}]}>
+              <View style={{backgroundColor: 'white'}}>
               <View style={{alignItems:'center'}}>
               <Image source={{uri:image}} style={{borderRadius: 4,height:SCREEN_HEIGHT*1/6, width:SCREEN_WIDTH * 1/2}}/>
             </View>
@@ -183,21 +197,45 @@ export default class ProductScreen extends React.Component {
               <Text style={{fontSize:18, fontWeight:"bold"}}>{name}</Text>
               <Text style={{fontSize:15, fontWeight:"bold", marginTop: 18}}>{price}</Text>
             </View>
-            <Icon
-              name="chevron-left"
-              size={30}
-              color={'green'}
-              onPress={()=>this.addToCart(this.state.item)}
-            />
+
+
+            </View>
+            <View style={{backgroundColor:'white'}}>
+
+            <View style={{alignItems:'flex-start', marginTop: 20, marginLeft:16}}>
+
+              <Text style={{fontSize: 12}}>Description: {this.state.description}</Text>
+            </View>
+          </View>
+
+
+
+
+
+
         </View>
+
         </Animated.ScrollView>
-        <Animated.View
+
+        {/* <Animated.View style={styles.header}>
+  <View style={styles.bar}>
+    <Text style={styles.title}>Title</Text>
+  </View>
+</Animated.View> */}
+<Animated.View
           pointerEvents="none"
           style={[
             styles.headerProduct,
             { opacity:imageOpacity,transform: [{ translateY: headerTranslate }] },
-          ]}>
+          ]}
+        >
+
+
+
+
        </Animated.View>
+
+
        <Animated.View
           style={[
             styles.behind,
@@ -211,20 +249,26 @@ export default class ProductScreen extends React.Component {
             },
           ]}
         >
-        <View style={{marginTop:-25,marginLeft: 15}}>
+          <View style={{marginTop:-25,marginLeft: 15}}>
+
         <TouchableOpacity style={{marginTop:-31.5,marginLeft:-1}} onPress={() => this.props.navigation.goBack()}>
           <Icon
                  name='chevron-left'
                  size={35}
                  color={'blue'}
                  underlayColor={'white'}
+
                />
              </TouchableOpacity>
            </View>
+
              <View style={{marginTop:-25,marginLeft: SCREEN_WIDTH*7/25, alignItem:'center'}}>
             <Text style={{fontSize:12, textAlign:'center', fontWeight:'bold', color:'black'}}>{this.state.name}</Text>
           </View>
+
           <View style={{marginTop:-25,marginLeft: 30}}>
+
+
           <TouchableOpacity style={{marginLeft:SCREEN_WIDTH - 64, marginTop:33}}>
               <Icon
               name='shopping-cart'
@@ -245,37 +289,39 @@ export default class ProductScreen extends React.Component {
                    {scale: titleScale }
                ],
              },
-           ]}>
+           ]}
+         >
+
            <View style={{flexDirection:'row'}}>
            <Card borderRadius={8} containerStyle={{height: 15,position:'absolute', top:-13,left:0, right:0, flexDirection:'row', alignItems:'flex-start', backgroundColor:'white', borderColor:'grey',marginLeft:6,width: SCREEN_WIDTH * 0.48}}>
          <TouchableOpacity style={{position:'absolute', top:-21,left:-10,right:SCREEN_WIDTH*1/3}} onPress={() => this.subtract()}>
            <Text style={{fontSize:32}}> - </Text>
               </TouchableOpacity>
-              <Text style={{position:'absolue', top:-13.75,left:67,right:0, fontSize: 22, fontWeight:'bold'}}>{this.state.quantity}</Text>
+              <Text style={{position:'absolue', top:-14,left:67,right:0, fontSize: 22, fontWeight:'bold'}}>{this.state.quantity}</Text>
               <TouchableOpacity style={{position:'absolute', top:-21,left:SCREEN_WIDTH*1/3,right:-10}} onPress={() => this.add()}>
                 <Text style={{fontSize:32}}> + </Text>
                    </TouchableOpacity>
+
           </Card>
-            <TouchableOpacity style={{borderRadius: 8,height:30,position:'absolute', top:13.5,left:SCREEN_WIDTH*15/29,right:0,zIndex:2,marginLeft:-2.5,marginTop:-10,
-            width: SCREEN_WIDTH * 0.48, backgroundColor:'red', alignItems:'center', justifyContent:'center',
+            <TouchableOpacity style={{alignItems:'center',borderRadius: 8,height:30,position:'absolute', top:2,left:SCREEN_WIDTH*15/29,right:0,zIndex:2,marginLeft:-2.5,
+            flexDirection:'row', width: SCREEN_WIDTH * 0.48, backgroundColor:'red',
             borderColor:'grey', marginRight:5}} onPress={()=>this.addToCart(item)}>
-           <Text style={{textAlign:'center',color: 'white', fontSize: 16}}> Add to Cart </Text>
+           <Text style={{marginLeft: 40,textAlign:'right',color: 'white', fontSize: 18, fontWeight:'bold'}}> Add to Cart </Text>
           </TouchableOpacity>
+
+
         </View>
-              <View style={{marginTop:-25,marginLeft: SCREEN_WIDTH*3/10}}>
-             <Text style={{fontSize:8, fontWeight:'bold', color:'black'}}>{this.state.name}</Text>
-           </View>
-           <View style={{marginTop:-25,marginLeft: 30}}>
-           <TouchableOpacity style={{marginLeft:SCREEN_WIDTH - 64, marginTop:33}}>
-               <Icon
-               name='shopping-cart'
-               color='blue'
-               onPress={()=>{this.props.screenProps.cart()}}/>
-             </TouchableOpacity>
-           </View>
+
+
+
          </Animated.View>
+
+
+
 </View>
 
     )
 }
 }
+
+export default ProductScreen;
